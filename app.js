22a8940a004f481b931ebe86f6f81a9dd616f8f8
Dhanush <dhanushbandi2005@gmail.com>
2026-01-26 22:59:22 +0530

    fix: update CORS origin to use environment variable and modify MongoDB connection settings

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import userRoutes from "./routes/user.routes.js"
 const app = express()
 
 app.use(cors({
-    origin: 'https://creat-r-client.vercel.app',
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
