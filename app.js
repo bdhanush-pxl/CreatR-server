bbbb8b661d8ced63e05c2f091e2220012874ea63
Dhanush <dhanushbandi2005@gmail.com>
2026-04-21 22:10:07 +0530

    fix:Changed the options value from wildcard to / match all the routes

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cors({
 }));
 
 // Ensure preflight requests are handled
-app.options("*", (req, res) => {
+app.options("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
